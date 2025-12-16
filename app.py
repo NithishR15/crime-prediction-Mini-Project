b8be7c4e4cbbe6cd658100c15392ef8e379ae407
Nithish <nithirio3@gmail.com>
2025-12-16 11:22:58 +0530

    Update: Refactored home route message in app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)  
 
 @app.route("/")
 def home():
     return "Crime Prediction System Running Online"
 
 if __name__ == "__main__":
-    app.run(host="http://localhost:5173/", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
